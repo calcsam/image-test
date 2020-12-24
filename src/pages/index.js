223c9cde31787bd1c1582fd7ef71c1419b62b27a
Sam Bhagwat <calcalcsam@gmail.com>
2020-12-23 23:06:36 -0500

    separate pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 import { StaticImage } from "gatsby-plugin-image"
 
 export const Dino = ({ i }) => (
-  <img width={100} src={`/gatsby-astronaut${i}.png`} alt="T-Rex" />
+  <StaticImage width={100} src={`../images/gatsby-astronaut${i}.png`} alt="T-Rex" />
 )
 
 
@@ -21,16 +21,17 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Dino i="" />
-    <Dino i="1" />
-    <Dino i="2" />
-    <Dino i="3" />
-    <Dino i="4" />
-    <Dino i="5" />
-    <Dino i="6" />
-    <Dino i="7" />
-    <Dino i="8" />
-    <Dino i="9" />
+    <StaticImage width={100} src="../images/gatsby-astronaut.png" alt="T-Rex" />    
+<StaticImage width={100} src="../images/gatsby-astronaut.png" alt="T-Rex" />
+<StaticImage width={100} src="../images/gatsby-astronaut1.png" alt="T-Rex" />
+<StaticImage width={100} src="../images/gatsby-astronaut2.png" alt="T-Rex" />
+<StaticImage width={100} src="../images/gatsby-astronaut3.png" alt="T-Rex" />
+<StaticImage width={100} src="../images/gatsby-astronaut4.png" alt="T-Rex" />
+<StaticImage width={100} src="../images/gatsby-astronaut5.png" alt="T-Rex" />
+<StaticImage width={100} src="../images/gatsby-astronaut6.png" alt="T-Rex" />
+<StaticImage width={100} src="../images/gatsby-astronaut7.png" alt="T-Rex" />
+<StaticImage width={100} src="../images/gatsby-astronaut8.png" alt="T-Rex" />
+<StaticImage width={100} src="../images/gatsby-astronaut9.png" alt="T-Rex" />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
