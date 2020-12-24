a0e985b0f11a5256469417713d4a29361c34ec28
Sam Bhagwat <calcalcsam@gmail.com>
2020-12-23 22:59:14 -0500

    diff souce files

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ import SEO from "../components/seo"
 
 import { StaticImage } from "gatsby-plugin-image"
 
-export const Dino = () => (
-  <img width={100} src="/gatsby-astronaut.png" alt="T-Rex" />
+export const Dino = ({ i }) => (
+  <img width={100} src={`/gatsby-astronaut${i}.png`} alt="T-Rex" />
 )
 
 
@@ -21,16 +21,16 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Dino />
-    <Dino />
-    <Dino />
-    <Dino />
-    <Dino />
-    <Dino />
-    <Dino />
-    <Dino />
-    <Dino />
-    <Dino />
+    <Dino i="" />
+    <Dino i="1" />
+    <Dino i="2" />
+    <Dino i="3" />
+    <Dino i="4" />
+    <Dino i="5" />
+    <Dino i="6" />
+    <Dino i="7" />
+    <Dino i="8" />
+    <Dino i="9" />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
